cb94d8301a4692ab4691bae1c62a046273494a9e
ArtemiiHH <artemiishchukin2003@gmail.com>
2026-06-29 21:45:03 +0300

    Fix flash where dashboard appears before login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,71 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import AddCase from './pages/AddCase'
-import CaseDetail from './pages/CaseDetail'
-import ClientTracking from './pages/ClientTracking'
-import Settings from './pages/Settings'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import AddCase from "./pages/AddCase";
+import CaseDetail from "./pages/CaseDetail";
+import ClientTracking from "./pages/ClientTracking";
+import Settings from "./pages/Settings";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"            element={<Navigate to="/dashboard" replace />} />
-        <Route path="/login"       element={<Login />} />
-        <Route path="/dashboard"          element={<Dashboard />} />
-        <Route path="/dashboard/active"   element={<Dashboard />} />
-        <Route path="/dashboard/resolved" element={<Dashboard />} />
-        <Route path="/add-case"    element={<AddCase />} />
-        <Route path="/cases/:id"   element={<CaseDetail />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/active"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/resolved"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-case"
+          element={
+            <ProtectedRoute>
+              <AddCase />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cases/:id"
+          element={
+            <ProtectedRoute>
+              <CaseDetail />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/case/:token" element={<ClientTracking />} />
-        <Route path="/settings"    element={<Settings />} />
-        <Route path="*"            element={<Navigate to="/dashboard" replace />} />
+        <Route
+          path="/settings"
+          element={
+            <ProtectedRoute>
+              <Settings />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
