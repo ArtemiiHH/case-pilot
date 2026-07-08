e1c137523c111656962fd3097df33e65d328944d
ArtemiiHH <artemiishchukin2003@gmail.com>
2026-07-08 13:17:39 +0300

    Change lax to none in sameSite

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   }),
