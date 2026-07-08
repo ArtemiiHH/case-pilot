4b0852deb1836a5eb3a3508f91263ce40327f179
ArtemiiHH <artemiishchukin2003@gmail.com>
2026-07-08 12:11:57 +0300

    Change none to lax in cookie

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   }),
